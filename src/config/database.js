311649ee1a061f65c74da84b2ccfda708f10d63d
Davi <davisilvaqueiroz@outlook.com>
2023-02-21 02:41:49 -0300

    alteração de banco para sqlite

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-  dialect: 'mariadb',
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
+  dialect: 'sqlite',
+  storage: './db.sqlite',
   define: {
     timestamps: true,
     underscored: true,
@@ -14,8 +10,4 @@ module.exports = {
     createdAt: 'created_at',
     updatedAt: 'updated_at',
   },
-  dialectOptions: {
-    timezone: 'America/Sao_Paulo',
-  },
-  timezone: 'America/Sao_Paulo',
 };
